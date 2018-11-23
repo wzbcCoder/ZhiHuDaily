fb86a66ae23d32f0b474f03dea8863f2e550cbbf
zhujingdi <34479567+xiantang@users.noreply.github.com>
2018-11-23 11:14:58 +0800

    添加了数据三层结构和数据库的操作

--- app/src/main/java/com/example/administrator/zhihunews/MainActivity.java
+++ app/src/main/java/com/example/administrator/zhihunews/MainActivity.java
@@ -1,23 +1,45 @@
 package com.example.administrator.zhihunews;
 
 import android.app.FragmentTransaction;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.administrator.zhihunews.db.until.DatabaseHelper;
+import com.example.administrator.zhihunews.db.until.DbManger;
 import com.example.administrator.zhihunews.fragment.NewsListFragment;
 
 public class MainActivity extends AppCompatActivity {
     FragmentManager mFragmentManger;
     // Fragment 事务
     FragmentTransaction mFragmentTransaction;
+    DatabaseHelper mMySqliteHelper;
+    private SQLiteDatabase db;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prepareDatabase();
         prepareFragment();
     }
+
+    private void prepareDatabase() {
+        mMySqliteHelper = DbManger.getIntance(MainActivity.this);
+        db = mMySqliteHelper.getWritableDatabase();
+//        ContentValues values = new ContentValues();
+//        values.put("ga_prefix",112309);
+//        values.put("id",9702141);
+//        values.put("image","https://pic3.zhimg.com/v2-8350fa5a9aadeb091d239ea1ecb9399a.jpg");
+//        values.put("title","什么样的报表真难看？拿这种好看的比一下恍然大悟");
+//        values.put("type",0);
+//        long rowid = db.insert(NewsItem.TABLE_NAME,null,values);
+//        System.out.println("ddd"+rowid);
+    }
+
     // 创建Fragment之前的操作
     private void prepareFragment(){
         // 通用的FragmentManager
@@ -27,5 +49,6 @@ public class MainActivity extends AppCompatActivity {
         mFragmentTransaction.replace(R.id.NewsListFragment,newsListFragment);
         // 提交
         mFragmentTransaction.commit();
+
     }
 }
--- app/src/main/java/com/example/administrator/zhihunews/app/ClintApplication.java
+++ app/src/main/java/com/example/administrator/zhihunews/app/ClintApplication.java
@@ -23,4 +23,5 @@ public class ClintApplication extends Application {
         return mRequestQueue;
     }
 
+
 }
