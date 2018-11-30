260387a06512acfd5623413413ace64514005973
lianglihao <42924541+lianglihao@users.noreply.github.com>
2018-11-30 11:52:50 +0800

    轮播图基本完成，但是软件闪退

--- app/src/main/java/com/example/administrator/zhihunews/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/zhihunews/activity/MainActivity.java
@@ -1,33 +1,33 @@
 package com.example.administrator.zhihunews.activity;
 import android.app.FragmentTransaction;
-import android.content.ClipData;
-import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Handler;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 //import com.example.administrator.zhihunews.db.until.DatabaseHelper;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.administrator.zhihunews.R;
+import com.example.administrator.zhihunews.adapter.InfoListAdapter;
+import com.example.administrator.zhihunews.db.model.NewsItem;
 import com.example.administrator.zhihunews.db.until.MyDatabaseHelper;
-import com.example.administrator.zhihunews.fragment.BaseFragment;
 import com.example.administrator.zhihunews.fragment.NewsListFragment;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+
+import cn.bingoogolapple.bgabanner.BGABanner;
 
 
 public class MainActivity extends BaseActivity {
@@ -37,8 +37,96 @@ public class MainActivity extends BaseActivity {
     MyDatabaseHelper mMySqliteHelper;
     private SQLiteDatabase db;
 
+    //NewsItem为自定义的，在NewsItem.java中实现
+    private ArrayList<NewsItem> bannerList;//banner控件
 
+    private ArrayList<String> titles;//存放banner中的标题
 
+    private ArrayList<String> images;//存放banner中的图片
+
+    private ArrayList<Integer> ids;//存放每一项的id
+
+    private RequestQueue mQueue;
+
+    private RecyclerView mInfoList;
+
+    private InfoListAdapter adapter;
+
+//    private void initBanner() {
+//        //初始化banner
+//        titles=new ArrayList<>();
+//        ids=new ArrayList<>();
+//        images=new ArrayList<>();
+//
+//        bannerList = new ArrayList<>();
+//
+//        mQueue = Volley.newRequestQueue(this);
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://news-at.zhihu.com/api/4/news/latest", null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                try {
+//                    //解析banner中的数据
+//                    JSONArray topinfos = response.getJSONArray("top_stories");
+//                    Log.d("TAG", "onResponse: "+topinfos);
+//                    for (int i = 0; i < topinfos.length(); i++) {
+//                        JSONObject item = topinfos.getJSONObject(i);
+//                        NewsItem item1 = new NewsItem();
+//                        item1.setImage(item.getString("image"));
+//                        item1.setTitle(item.getString("title"));
+//                        item1.setId(item.getInt("id"));
+//                        bannerList.add(item1);
+//                        titles.add(item1.getTitle());
+//                        images.add(item1.getImage());
+//                        ids.add(item1.getId());
+//                    }
+//
+//
+//                    setHeader(mInfoList, images, titles, ids);
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        }, new Response.ErrorListener() {
+//
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        });
+//        mQueue.add(jsonObjectRequest);
+//
+//
+//    }
+//
+//    private void setHeader(RecyclerView view, ArrayList<String> urls, ArrayList<String> titles, final ArrayList<Integer> ids) {
+//        View header = LayoutInflater.from(this).inflate(R.layout.headview, view, false);
+//        //找到banner所在的布局
+//        BGABanner banner = (BGABanner) header.findViewById(R.id.banner);
+//        //绑定banner
+//        banner.setAdapter(new BGABanner.Adapter<ImageView, String>() {
+//
+//
+//            @Override
+//            public void fillBannerItem(BGABanner banner, ImageView itemView, String model, int position) {
+//                Glide.with(MainActivity.this)
+//                        .load(model)
+//                        .centerCrop()
+//                        .dontAnimate()
+//                        .into(itemView);
+//            }
+//        });
+//        banner.setDelegate(new BGABanner.Delegate() {
+//            @Override
+//            public void onBannerItemClick(BGABanner banner, View itemView, Object model, int position) {
+//                //此处可设置banner子项的点击事件
+//
+//            }
+//        });
+//        banner.setData(urls, titles);
+//        adapter.setHeadView(header);//向适配器中添加banner
+//    }
 
 
 
@@ -79,8 +167,6 @@ public class MainActivity extends BaseActivity {
 //        values.put("type",0);
 //        long rowid = db.insert(NewsItem.TABLE_NAME,null,values);
 //        System.out.println("ddd"+rowid);
-
-
 
 
 
--- app/src/main/java/com/example/administrator/zhihunews/fragment/NewsListFragment.java
+++ app/src/main/java/com/example/administrator/zhihunews/fragment/NewsListFragment.java
@@ -1,19 +1,25 @@
 package com.example.administrator.zhihunews.fragment;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ImageView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.administrator.zhihunews.R;
 import com.example.administrator.zhihunews.activity.MainActivity;
 import com.example.administrator.zhihunews.adapter.InfoListAdapter;
@@ -21,14 +27,17 @@ import com.example.administrator.zhihunews.app.ClintApplication;
 import com.example.administrator.zhihunews.db.daoImp.NewsItemDaoImp;
 import com.example.administrator.zhihunews.db.model.NewsItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import cn.bingoogolapple.bgabanner.BGABanner;
 
 /**
  * Created by Administrator on 2018/11/23.
@@ -42,6 +51,97 @@ public class NewsListFragment extends BaseFragment {
     private ArrayList<NewsItem> mDatas;
     private RecyclerView mInfoList;
     private InfoListAdapter adapter;
+
+    private ArrayList<NewsItem> bannerList;//banner控件
+
+    private ArrayList<String> titles;//存放banner中的标题
+
+    private ArrayList<String> images;//存放banner中的图片
+
+    private ArrayList<Integer> ids;//存放每一项的id
+
+    private RequestQueue mQueue;
+
+    private void initBanner() {
+        //初始化banner
+        titles=new ArrayList<>();
+        ids=new ArrayList<>();
+        images=new ArrayList<>();
+
+        bannerList = new ArrayList<>();
+
+        mQueue = Volley.newRequestQueue(mActivity);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://news-at.zhihu.com/api/4/news/latest", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    //解析banner中的数据
+                    JSONArray topinfos = response.getJSONArray("top_stories");
+                    Log.d("TAG", "onResponse: "+topinfos);
+                    for (int i = 0; i < topinfos.length(); i++) {
+                        JSONObject item = topinfos.getJSONObject(i);
+                        NewsItem item1 = new NewsItem();
+                        item1.setImage(item.getString("image"));
+                        item1.setTitle(item.getString("title"));
+                        item1.setId(item.getInt("id"));
+                        bannerList.add(item1);
+                        titles.add(item1.getTitle());
+                        images.add(item1.getImage());
+                        ids.add(item1.getId());
+                    }
+
+
+                    setHeader(mInfoList, images, titles, ids);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        mQueue.add(jsonObjectRequest);
+
+
+    }
+
+    private void setHeader(RecyclerView view, ArrayList<String> urls, ArrayList<String> titles, final ArrayList<Integer> ids) {
+        View header = LayoutInflater.from(mActivity).inflate(R.layout.headview, view, false);
+        //找到banner所在的布局
+        BGABanner banner = (BGABanner) header.findViewById(R.id.banner);
+        //绑定banner
+        banner.setAdapter(new BGABanner.Adapter<ImageView, String>() {
+
+
+            @Override
+            public void fillBannerItem(BGABanner banner, ImageView itemView, String model, int position) {
+                Glide.with(NewsListFragment.this)
+                        .load(model)
+                        .centerCrop()
+                        .dontAnimate()
+                        .into(itemView);
+            }
+        });
+        banner.setDelegate(new BGABanner.Delegate() {
+            @Override
+            public void onBannerItemClick(BGABanner banner, View itemView, Object model, int position) {
+                //此处可设置banner子项的点击事件
+
+            }
+        });
+        banner.setData(urls, titles);
+        adapter.setHeadView(header);//向适配器中添加banner
+    }
+
+    ///
+
+
+
 
     // 初始化视图
     @Override
@@ -57,6 +157,8 @@ public class NewsListFragment extends BaseFragment {
         mInfoList = (RecyclerView) mActivity.findViewById(R.id.infolist);//绑定RecycleView
         mInfoList.setLayoutManager(new LinearLayoutManager(mActivity));//设置布局管理器，你可以通过这个来决定你是要做一个Listview还是瀑布流
         adapter = new InfoListAdapter(mDatas, mActivity);//初始化适配器
+
+
         mInfoList.setAdapter(adapter);//为ReycleView设置适配器
     }
 
@@ -70,6 +172,7 @@ public class NewsListFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
         initData();
         initView();
+        initBanner();
     }
     //    @Override
 //    public void onActivityCreated(Bundle savedInstanceState) {
